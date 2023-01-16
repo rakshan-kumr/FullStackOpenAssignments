59bd62a02665d92dc1c15f2ae8e3e84d6ab5171c
rakshan-kumr <rkshnkumr@gmail.com>
2023-01-16 21:41:00 +0530

    Excericise moved to correct folder.

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodClick = () => setGood(good + 1)
+  const neutralClick = () => setNeutral(neutral + 1)
+  const badClick = () => setBad(bad + 1)
+
+
+
+  return (
+    <div>
+      <Header text="Give Feedback" />
+      <Button handleClick={goodClick} text="Good" />
+      <Button handleClick={neutralClick} text="Neutral" />
+      <Button handleClick={badClick} text="Bad" />
+      <Header text="Statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Header = ({ text }) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good * 1 + bad * -1) / all}</p>
+      <p>positive {good / all * 100} %</p>
+    </div>
+  )
+}
+
+export default App
