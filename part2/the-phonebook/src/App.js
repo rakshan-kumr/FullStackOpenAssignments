56925f5eba2da13c881650ccc94132aa3b2a27aa
rakshan-kumr <rkshnkumr@gmail.com>
2023-01-19 11:31:54 +0530

    2.11 PhoneBook Step6 Server

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState("")
   const [filterValue, setFilterValue] = useState("")
+
+  const url = "http://localhost:3001/persons"
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -59,10 +65,10 @@ const App = () => {
         onNameChange={nameChangeHandler}
         newNum={newNum}
         onNumChange={numChangeHandler} />
-        
+
       <h3>Number</h3>
 
-      <Persons persons={personsToShow}/>
+      <Persons persons={personsToShow} />
     </div>
   )
 }
