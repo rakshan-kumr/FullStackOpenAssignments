f3a78e10b3f98a0b4fc609d7dfb76689d5dde0da
rakshan-kumr <rkshnkumr@gmail.com>
2023-02-02 10:10:15 +0530

    3.9 Fixed POST Response

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,115 +1,123 @@
-import { useState, useEffect } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import phonebookService from './services/communications'
-import Notification from './components/Notification'
+import { useState, useEffect } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import phonebookService from "./services/communications";
+import Notification from "./components/Notification";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNum, setNewNum] = useState("")
-  const [filterValue, setFilterValue] = useState("")
-  const [resultMessage, setresultMessage] = useState(null)
-  const [messageType, setMessagetype] = useState('')
-
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNum, setNewNum] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const [resultMessage, setresultMessage] = useState(null);
+  const [messageType, setMessagetype] = useState("");
 
   useEffect(() => {
-    phonebookService
-      .getAllPerson()
-      .then(allPersonData => {
-        setPersons(allPersonData)
-      })
-  }, [])
+    phonebookService.getAllPerson().then((allPersonData) => {
+      setPersons(allPersonData);
+    });
+  }, []);
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const getAllPersonData = phonebookService.getAllPerson()
+    const getAllPersonData = phonebookService.getAllPerson();
 
-    getAllPersonData
-      .then(allPersonData => {
-        const isExist = allPersonData.some(person => person.name === newName)
+    getAllPersonData.then((allPersonData) => {
+      const isExist = allPersonData.some((person) => person.name === newName);
 
-        if (!isExist) {
-          const personObject = {
-            name: newName,
-            number: newNum,
-          }
+      if (!isExist) {
+        const personObject = {
+          name: newName,
+          number: newNum,
+        };
 
-          phonebookService
-            .createPerson(personObject)
-            .then(response => {
-              setPersons(persons.concat(response.data))
-              setMessagetype('success-message')
-              setresultMessage(`Added ${personObject.name}`)
-              setTimeout(()=> {
-                setresultMessage(null)
-              }, 5000)
-              setNewName("")
-              setNewNum("")
+        phonebookService.createPerson(personObject).then((response) => {
+          console.log("Persons: ", persons);
+          console.log("response.data: ", response.data);
+          setPersons(response.data);
+          setMessagetype("success-message");
+          setresultMessage(`Added ${personObject.name}`);
+          setTimeout(() => {
+            setresultMessage(null);
+          }, 5000);
+          setNewName("");
+          setNewNum("");
+        });
+      } else {
+        const addConfirm = window.confirm(
+          `${newName} is already added to phonebook, replace the old number with new one?`
+        );
+        if (addConfirm) {
+          getAllPersonData.then((allPersonData) => {
+            const personObject = allPersonData.find(
+              (person) => person.name === newName
+            );
+            const newPersonObject = { ...personObject, number: newNum };
 
-            })
-
-        } else {
-          const addConfirm = window.confirm(`${newName} is already added to phonebook, replace the old number with new one?`)
-          if (addConfirm) {
-            getAllPersonData.then(allPersonData => {
-              const personObject = allPersonData.find(person => person.name === newName)
-              const newPersonObject = { ...personObject, number: newNum }
-
-              phonebookService
-                .updatePerson(newPersonObject.id, newPersonObject)
-                .then(returnedPersonObject => {
-                  setPersons(persons
-                    .map(person => person.id === newPersonObject.id ? returnedPersonObject : person))
-                })
-                setresultMessage(`Added ${personObject.name}`)
-                setTimeout(()=> {
-                  setresultMessage(null)
-                }, 5000)
-                setNewName("")
-                setNewNum("")
-            })
-          }
+            phonebookService
+              .updatePerson(newPersonObject.id, newPersonObject)
+              .then((returnedPersonObject) => {
+                setPersons(
+                  persons.map((person) =>
+                    person.id === newPersonObject.id
+                      ? returnedPersonObject
+                      : person
+                  )
+                );
+              });
+            setresultMessage(`Added ${personObject.name}`);
+            setTimeout(() => {
+              setresultMessage(null);
+            }, 5000);
+            setNewName("");
+            setNewNum("");
+          });
         }
-      })
-  }
+      }
+    });
+  };
 
   const deletePersonAction = (person) => {
     phonebookService
       .deletePerson(person)
-      .then(() => phonebookService
-        .getAllPerson()
-        .then(allPersonData => setPersons(allPersonData)))
-        .catch(error => {
-          setresultMessage(`Information of ${person.name} has already been removed form server`)
-          setMessagetype('error-message')
-          setTimeout(() => {
-            setresultMessage(null)
-          }, 5000);
-        })
-
-  }
+      .then(() =>
+        phonebookService
+          .getAllPerson()
+          .then((allPersonData) => setPersons(allPersonData))
+      )
+      .catch((error) => {
+        setresultMessage(
+          `Information of ${person.name} has already been removed form server`
+        );
+        setMessagetype("error-message");
+        setTimeout(() => {
+          setresultMessage(null);
+        }, 5000);
+      });
+  };
 
   const nameChangeHandler = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
   const numChangeHandler = (event) => {
-    setNewNum(event.target.value)
-  }
+    setNewNum(event.target.value);
+  };
 
   const filterHandler = (event) => {
-    setFilterValue(event.target.value)
-  }
+    setFilterValue(event.target.value);
+  };
 
-  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(filterValue.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Phonebook</h1>
 
-      <Notification message={resultMessage} messageType={messageType}/>
+      <Notification message={resultMessage} messageType={messageType} />
 
       <Filter value={filterValue} onChange={filterHandler} />
 
@@ -120,13 +128,14 @@ const App = () => {
         newName={newName}
         onNameChange={nameChangeHandler}
         newNum={newNum}
-        onNumChange={numChangeHandler} />
+        onNumChange={numChangeHandler}
+      />
 
       <h3>Number</h3>
 
       <Persons persons={personsToShow} deletePerson={deletePersonAction} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
