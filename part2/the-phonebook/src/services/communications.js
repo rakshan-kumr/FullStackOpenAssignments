9010bd37bcd02cbe7d86443ea7eccb49bb861f03
rakshan-kumr <rkshnkumr@gmail.com>
2023-02-02 09:10:33 +0530

    3.9 Phonebook Backend Step9

--- part2/the-phonebook/src/services/communications.js
+++ part2/the-phonebook/src/services/communications.js
@@ -1,35 +1,33 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "api/persons";
 
 const getAllPerson = () => {
-    console.log('getAllPerson Function fired!');
-    return axios
-        .get(baseUrl)
-        .then(response => response.data)
-}
+  console.log("getAllPerson Function fired!");
+  return axios.get(baseUrl).then((response) => {
+    console.log(response.data);
+    return response.data;
+  });
+};
 
-const createPerson = newPerson => {
-    console.log('createPerson Function fired!');
-    return axios
-        .post(baseUrl, newPerson)
-}
+const createPerson = (newPerson) => {
+  console.log("createPerson Function fired!");
+  return axios.post(baseUrl, newPerson);
+};
 
 const updatePerson = (id, newPersonObject) => {
-    console.log('updatePerson Function fired!');
-    return axios
-        .put(`${baseUrl}/${id}`, newPersonObject)
-        .then(response => response.data)
-}
+  console.log("updatePerson Function fired!");
+  return axios
+    .put(`${baseUrl}/${id}`, newPersonObject)
+    .then((response) => response.data);
+};
 
-const deletePerson = person => {
-    const canConfirm = window.confirm(`Delete ${person.name} ?`)
-    if (canConfirm) {
-        console.log('DeletePerson function fired!');
-        return axios
-            .delete(`${baseUrl}/${person.id}`)
-    }
-}
+const deletePerson = (person) => {
+  const canConfirm = window.confirm(`Delete ${person.name} ?`);
+  if (canConfirm) {
+    console.log("DeletePerson function fired!");
+    return axios.delete(`${baseUrl}/${person.id}`);
+  }
+};
 
-
-export default { getAllPerson, createPerson, deletePerson, updatePerson }
+export default { getAllPerson, createPerson, deletePerson, updatePerson };
