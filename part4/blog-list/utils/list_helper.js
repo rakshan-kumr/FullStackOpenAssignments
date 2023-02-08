a3ca11a0904f91f2f8c05afda4e15abfa090ca66
rakshan-kumr <rkshnkumr@gmail.com>
2023-02-08 14:18:07 +0530

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((prev, current) => prev + current.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  let maxLike =  Math.max(...blogs.map((blog) => blog.likes))
+  return blogs.find(blog => blog.likes === maxLike)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
