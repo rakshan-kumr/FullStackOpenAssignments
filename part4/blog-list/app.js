c56fb95f221b34364c5fdaf8003473fb50ab6f89
rakshan-kumr <rkshnkumr@gmail.com>
2023-06-07 23:19:04 +0530

    4.15: bloglist expansion, step3

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/bloglist')
+const usersRouter = require('./controllers/users')
+
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
@@ -24,5 +26,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
+
 
 module.exports = app
