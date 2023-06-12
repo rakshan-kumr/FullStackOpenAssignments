d4656a2620191ca368016e2f33430a43b11bac61
rakshan-kumr <rkshnkumr@gmail.com>
2023-06-12 08:36:29 +0530

    4.18: bloglist expansion, step6

--- part4/blog-list/controllers/bloglist.js
+++ part4/blog-list/controllers/bloglist.js
@@ -1,15 +1,31 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user' , { name: 1, username: 1 })
   response.json(blogs)
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.split(' ')[1]
+  }
+  return null
+}
+
 blogRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  console.log(decodedToken)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
